453b00d67a80191e0ce5300ee97f4c597bb2af87
adrian001121 <adrianyikiat@student.unimelb.edu.au>
2023-04-03 15:09:29 +1000

    Successfully develop search features

--- spotify-app/src/components/Dashboard.js
+++ spotify-app/src/components/Dashboard.js
@@ -1,38 +1,112 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import AuthAgent from "./AuthAgent";
+// import SpotifyWebApi from "spotify-web-api-node";
+
+// const spotifyCredentials = {
+//     clientId: 'ee859872f4354d5093bba8275dd2ace1',
+//     clientSecret: 'a38b3dae7f6b47669a9f4d3e0bb9ba2b',
+//     redirectUri: 'http://localhost:3000'
+// };
 
 const Dashboard = ({ code }) => {
+  const accessToken = AuthAgent(code);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-    const accessToken = AuthAgent(code);
-    const [search, setSearch] = useState("");
+  // const spotifyApi = new SpotifyWebApi({spotifyCredentials});
 
-    const submitHandler = (e)=>{
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setSearch(e.target.search.value);
+  };
 
-        e.preventDefault();
-        setSearch(e.target.search.value);
+  // //Access token change
+  // UseEffect(() => {
+
+  //     if(!accessToken){
+  //         return;
+  //     }
+
+  //     spotifyApi.setAccessToken(accessToken);
+
+  // }, [accessToken]);
+
+  //Search
+  useEffect(() => {
+    if (!search || !accessToken) {
+      return setSearchResults([]);
     }
 
+    async function Search() {
+      try {
+        const response = await fetch("http://localhost:8888/search", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            accessToken: accessToken,
+            searchContent: search,
+          }),
+        });
 
-    return (
-        <div className="Container d-flex flex-column py-2" style={{ height: "100vh" }}>
-            <div className="Search">
-                <form onSubmit={submitHandler}>
-                    <div className="form-row">
-                        <div className="form-group col-lg-4 mr-3">
-                            <input className="form-control" name="search"  defaultValue={search} placeholder="Search Songs/Artists" minLength="2" required/>
-                        </div>
-                        <div className="form-group">
-                            <button className="btn btn-outline-primary" style={{transform: 'translateY(10px)'}} type="submit">Search</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+        const data = await response.json();
 
-            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-                {search}
+        setSearchResults(
+          data.map((tracks) => {
+            return {
+              id: tracks.id,
+              name: tracks.name,
+              artist: tracks.artists[0].name,
+              album: tracks.album.name,
+            };
+          })
+        );
+
+        console.log(searchResults);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    Search();
+  }, [search, accessToken]);
+
+  return (
+    <div
+      className="Container d-flex flex-column py-2"
+      style={{ height: "100vh" }}
+    >
+      <div className="Search">
+        <form onSubmit={submitHandler}>
+          <div className="form-row">
+            <div className="form-group col-lg-4 mr-3">
+              <input
+                className="form-control"
+                name="search"
+                defaultValue={search}
+                placeholder="Search Songs/Artists"
+                minLength="2"
+                required
+              />
             </div>
-        </div>
-    );
+            <div className="form-group">
+              <button
+                className="btn btn-outline-primary"
+                style={{ transform: "translateY(10px)" }}
+                type="submit"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}></div>
+      <p> {searchResults[0].name} </p>
+    </div>
+  );
 };
 
 export default Dashboard;
--- spotify-server/server.js
+++ spotify-server/server.js
@@ -1,61 +1,84 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const spotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const credentials = {
+    clientId: "ee859872f4354d5093bba8275dd2ace1",
+    clientSecret: "a38b3dae7f6b47669a9f4d3e0bb9ba2b",
+    redirectUri: "http://localhost:3000",
+    refreshToken: refreshToken,
+  };
+  const spotifyApi = new spotifyWebApi(credentials);
 
-app.post('/refresh', (req, res) => {
-    const refreshToken = req.body.refreshToken;
-    const credentials = {
-        clientId: 'ee859872f4354d5093bba8275dd2ace1',
-        clientSecret: 'a38b3dae7f6b47669a9f4d3e0bb9ba2b',
-        redirectUri: 'http://localhost:3000',
-        refreshToken: refreshToken
-    };
-    const spotifyApi = new spotifyWebApi(credentials);
-
-    spotifyApi
-        .refreshAccessToken()
-        .then(data => {
-            res.json({
-                accessToken: data.body.access_token,
-                expiresIn: data.body.expires_in
-            })
-        })
-        .catch(err => {res.sendStatus(400); console.log(err);});
-
-
-})
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+      console.log(err);
+    });
+});
 
 //own app server
 // post request for login
-app.post('/login', (req, res) => {
-    const code = (req.body.code+'').trim(); // link to the web address "code"
-    const credentials = {
-        clientId: 'ee859872f4354d5093bba8275dd2ace1',
-        clientSecret: 'a38b3dae7f6b47669a9f4d3e0bb9ba2b',
-        redirectUri: 'http://localhost:3000'
-    };
-    const spotifyApi = new spotifyWebApi(credentials);
+app.post("/login", (req, res) => {
+  const code = (req.body.code + "").trim(); // link to the web address "code"
+  const credentials = {
+    clientId: "ee859872f4354d5093bba8275dd2ace1",
+    clientSecret: "a38b3dae7f6b47669a9f4d3e0bb9ba2b",
+    redirectUri: "http://localhost:3000",
+  };
+  const spotifyApi = new spotifyWebApi(credentials);
 
-    spotifyApi
-        .authorizationCodeGrant(code)
-        .then(data => {
-            res.json({
-                accessToken: data.body.access_token,
-                refreshToken: data.body.refresh_token,
-                expiresIn: data.body.expires_in
-            })
-        })
-        .catch(err => {
-            // console.log(err);
-        })
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      // console.log(err);
+    });
+});
 
+app.post("/search", (req, res) => {
+  const accessToken = req.body.accessToken; // link to the web address "accesstoken"
+  const searchContent = req.body.searchContent; // link to the web address "searchContent"
+
+  const credentials = {
+    clientId: "ee859872f4354d5093bba8275dd2ace1",
+    clientSecret: "a38b3dae7f6b47669a9f4d3e0bb9ba2b",
+    redirectUri: "http://localhost:3000",
+  };
+
+  const spotifyApi = new spotifyWebApi(credentials);
+
+  spotifyApi.setAccessToken(accessToken);
+
+  spotifyApi
+    .searchTracks(searchContent)
+    .then((data) => {
+      res.json(data.body.tracks.items);
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+    });
 });
 
 app.listen(8888);
