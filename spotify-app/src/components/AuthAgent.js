611f791c03533f37a01f5b6ef2ff00658ab2d76b
adrian001121 <adrianyikiat@student.unimelb.edu.au>
2023-04-04 14:28:30 +1000

    Change on Auth Agent

--- spotify-app/src/components/AuthAgent.js
+++ spotify-app/src/components/AuthAgent.js
@@ -1,73 +1,64 @@
-import { useState,useEffect } from "react";;
+import { useState, useEffect } from "react";
 
+function AuthAgent(code) {
+  const [accessToken, setAccessToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
+  const [expiresIn, setExpiresIn] = useState("");
 
-function AuthAgent( code ) {
+  useEffect(() => {
+    async function fetchLoginData() {
+      try {
+        const response = await fetch("http://localhost:8888/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ code: code }),
+        });
+        const data = await response.json();
+        setAccessToken(data.accessToken);
+        setRefreshToken(data.refreshToken);
+        setExpiresIn(data.expiresIn);
+        window.history.pushState({}, null, "/");
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
-    const [accessToken, setAccessToken] = useState("");
-    const [refreshToken, setRefreshToken] = useState("");
-    const [expiresIn, setExpiresIn] = useState("");
+    fetchLoginData();
+  }, [code]);
 
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) {
+      return;
+    }
 
-    useEffect(() => {
+    async function fetchRefreshToken() {
+      try {
+        const response = await fetch("http://localhost:8888/refresh", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ refreshToken: refreshToken }),
+        });
+        const data = await response.json();
+        setAccessToken(data.accessToken);
+        setExpiresIn(data.expiresIn);
+        window.history.pushState({}, null, "/");
+      } catch (error) {
+        window.location = "/";
+      }
+    }
 
-        async function fetchLoginData() {
-          try {
-            const response = await fetch('http://localhost:8888/login', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({ code: code })
-            });
-            const data = await response.json();
-            setAccessToken(data.accessToken);
-            setRefreshToken(data.refreshToken);
-            setExpiresIn(data.expiresIn);
-            window.history.pushState({}, null, "/");
-          } catch (error) {
-            console.error(error);
-          }
-        }
+    const interval = setInterval(() => {
+      fetchRefreshToken();
+    }, (expiresIn - 60) * 1000);
 
-        fetchLoginData();
-    }, [code]);
+    return () => clearInterval(interval); //clearInterval makes sure the interval is cleared when the component unmounts;
+  }, [refreshToken, expiresIn]);
 
-    useEffect(() => {
-
-        if (!refreshToken || !expiresIn) {
-            return;
-        }
-
-        async function fetchRefreshToken() {
-            try {
-                const response = await fetch('http://localhost:8888/refresh', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ refreshToken: refreshToken })
-                });
-                const data = await response.json();
-                setAccessToken(data.accessToken);
-                setExpiresIn(data.expiresIn);
-                window.history.pushState({}, null, "/");
-
-            } catch (error) {
-
-                window.location = "/";
-            }
-        }
-
-        const interval = setInterval(() => {
-            fetchRefreshToken();
-        }, (expiresIn - 60) * 1000);
-
-        return () => clearInterval(interval); //clearInterval makes sure the interval is cleared when the component unmounts;
-
-    }, [refreshToken, expiresIn]);
-
-    return accessToken;
-
+  return accessToken;
 }
 
 export default AuthAgent;
